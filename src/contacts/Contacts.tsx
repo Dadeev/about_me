468f39fe523622638b3cced9ef86be7ac0db85fe
Tima <dadeev.tim@gmail.com>
2022-10-09 13:27:54 +0700

    feat: implement basic layout Contact block

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import style from './Contacts.module.css'
 import styleContainer from '../common/styles/Container.module.css'
+import {Title} from "../common/components/Title/Title";
 
 export const Contacts = () => {
     return (
         <div className={style.contactsBlock}>
             <div className={`${styleContainer.container} ${style.blockContainer}`}>
-                <div className={style.contactsWrap}>
-                    <h2 className={style.title}>Contact</h2>
-                    <form className={style.contact} action="#" autoComplete={'on'}>
-                        <input className={style.input} type="text" placeholder={"What's your name?"} required/>
-                        <input className={style.input} type="email" placeholder={"Your Email"} required/>
-                        <textarea className={style.textArea} placeholder={"Let's chat (:"} required></textarea>
-                    </form>
-                    <button className={style.button}>Submit</button>
-                </div>
+                <Title subTitle={''} title={'Contact me'}/>
+                <form className={style.contact} action="#" autoComplete={'on'}>
+                    <div className={style.inputs}>
+                        <div className={style.InputContainer}>
+                            <input className={style.input} type="text" required/>
+                            <span className={style.ContactSpan}>Email*</span>
+                        </div>
+                        <div className={style.InputContainer}>
+                            <input className={style.input} type="email" required/>
+                            <span className={style.ContactSpan}>Name*</span>
+                        </div>
+                    </div>
+                    <div className={style.InputContainer}>
+                        <input required type={'text'} className={style.input}/>
+                        <span className={style.ContactSpan}>Phone</span>
+                    </div>
+                </form>
+                <button className={style.button}>Send me a message</button>
             </div>
         </div>
     );

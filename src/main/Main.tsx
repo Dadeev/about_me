a24bedf04a509d646cdd35e1c2b01273c771fd2f
Tima <dadeev.tim@gmail.com>
2022-10-16 17:26:18 +0700

    feat: implement realization of colors of the main block

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import style from './Main.module.css'
 import StyleContainer from '../common/styles/Container.module.css'
+import photo from "../assets/image/cat.jpg";
 
 export const Main = () => {
+    const trello = {
+        backgroundImage: `url(${photo})`
+    }
     return (
         <div className={style.mainBlock}>
             <div className={StyleContainer.container}>
-                <div className={style.text}>
-                    <span>Hi There!</span>
-                    <h1>I am Timothey Dadeev</h1>
-                    <p>Frontend Developer</p>
-                </div>
-                <div className={style.photo}>
-
-                </div>
+                <h1 className={style.text}>
+                    Hi, I’m
+                    <span> Timothey Dadeev</span>
+                    <br/>
+                    <div className={style.position}>a Front-end Developer</div>
+                </h1>
+                <div className={style.photo} style={trello}></div>
             </div>
         </div>
     );

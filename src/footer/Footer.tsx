7f0ec582f8d90dc32e83b8ff609f04aef5178228
Tima <dadeev.tim@gmail.com>
2022-10-16 18:13:07 +0700

    feat: implement realization of colors of the footer block

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import style from './Footer.module.css'
 import styleContainer from '../common/styles/Container.module.css'
+import linkedIn from "../assets/image/LinkedIn_logo_initials.png";
+import github from "../assets/image/Octocat.png";
+import telegram from "../assets/image/Telegram_logo.svg.webp";
 
 export const Footer = () => {
+    const linkedInPhoto = {
+        backgroundImage: `url(${linkedIn})`
+    }
+    const githubPhoto = {
+        backgroundImage: `url(${github})`
+    }
+    const telegramPhoto = {
+        backgroundImage: `url(${telegram})`
+    }
+
     return (
         <div className={style.footerBlock}>
             <div className={`${styleContainer.container} ${style.footerContainer}`}>
                 <div className={style.contacts}>
                     <h2 className={style.title}>Timothey Dadeev</h2>
                     <div className={style.icons}>
-                        <div className={style.icon}></div>
-                        <div className={style.icon}></div>
-                        <div className={style.icon}></div>
-                        <div className={style.icon}></div>
+                        <a href='https://vk.com/naav1s' target='_blank' className={style.icon} style={linkedInPhoto}></a>
+                        <a href='https://github.com/Dadeev' className={style.icon} target='_blank' style={githubPhoto}></a>
+                        <a href='https://t.me/Tima_Dadeev' className={style.icon} target='_blank' style={telegramPhoto}></a>
                     </div>
                     <span className={style.rights}>©2022 All rights reserved</span>
                 </div>
